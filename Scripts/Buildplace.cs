c98bab86b8b6c08aa1b283c590e4ee3cc3c90156
JustinTurnau <59576679+JustinTurnau@users.noreply.github.com>
2020-08-30 16:55:28 -0400

    Add files via upload

---
+++ Scripts/Buildplace.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Buildplace : MonoBehaviour
+{
+    // The tower that should be built
+    public GameObject towerPrefab;
+
+    void OnMouseUpAsButton() {
+        // Build tower above Buildplace
+        GameObject g = (GameObject)Instantiate(towerPrefab);
+        g.transform.position = transform.position + Vector3.up;
+    }
+
+}
